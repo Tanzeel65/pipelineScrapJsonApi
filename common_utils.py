8c66c620b51bab21deaa818db96b0fa46b40b853
Tanzeel65 <162446378+Tanzeel65@users.noreply.github.com>
2024-03-12 13:08:28 +0530

    Add files via upload

---
+++ common_utils.py
@@ -0,0 +1,19 @@
+
+import requests
+
+
+
+# Function for getting file_status using mlNewId
+def file_status(mlNewId):
+    print(mlNewId)
+
+    url = f"http://49.249.168.190:7000/file_status/{mlNewId}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print("Data sent successfully")
+
+        return response.status_code,response.json()
+    else:
+        print("Failed to send data. Status code:", response.status_code)
+        return f"failed to send data error: {response.status_code}"
